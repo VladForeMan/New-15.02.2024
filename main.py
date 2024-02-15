8ee04ad0e277321689346ee161c147d35c5a1b06
student <student@itstep1.dp.ua>
2024-02-15 19:57:32 +0200

    Returned in back

---
+++ main.py
@@ -0,0 +1,13 @@
+min_value = int(input("Введіть першу межу діапазону: "))
+max_value = int(input("Введіть другу межу діапазону: "))
+while True:
+  number = int(input("Введіть число: "))
+  if min_value <= number <= max_value:
+    break
+  print("Число не входить в діапазон. Повторіть введення")
+for i in range(min_value, max_value + 1):
+  if i == number:
+    print(f"!{i}!", end=" ")
+  else:
+    print(i, end=" ")
+print("\n")
