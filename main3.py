3bd42daaf61b6c9df7320844d3717259c8013849
student <mod.saleroot@gmail.com>
2024-02-15 20:47:31 +0200

    for, in, if, elif

--- main3.py
+++ main3.py
@@ -1,3 +1,8 @@
 string = input("Введіть рядок: ")
 letter_count = 0
 digit_count = 0
+for character in string:
+    if character.isalpha():
+        letter_count +=1
+    elif character.isdigit():
+        digit_count += 1
