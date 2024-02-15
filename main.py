713d996a9ea0bc8aba7c678e07e7b60cc740e246
student <student@itstep1.dp.ua>
2024-02-15 18:53:54 +0200

    add 8-12 line

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ while True:
     if min_value <= number <= max_value:
         break
     print("Число не входить в діапазон. Повторіть введення")
+    for i in range(min_value, max_value + 1):
+        if i == number:
+            print(f"!{i}!", end=" ")
+        else:
+            print(i, end=" ")
