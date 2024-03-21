54fcdcac0b95240ed0fb77e7797aaa9ba4104ec6
student <mod.saleroot@gmail.com>
2024-03-21 19:01:13 +0200

    22/02.2024(2)

--- 21.03.2024(2).py
+++ 21.03.2024(2).py
@@ -1,8 +1,17 @@
+# def fa_pow(x, n):
+#     if n == 0:
+#         return 1
+#     else:
+#         return x * fa_pow(x, n - 1)
+#
+# print(fa_pow(2, 3))
+# print(fa_pow(5, 2))
+# print(fa_pow(-2, 3))
+'''______________________________________________________________'''
 def fa_pow(x, n):
     if n == 0:
         return 1
-    else:
-        return x * fa_pow(x, n - 1)
+    return x * fa_pow(x, n - 1)
 
 print(fa_pow(2, 3))
 print(fa_pow(5, 2))
